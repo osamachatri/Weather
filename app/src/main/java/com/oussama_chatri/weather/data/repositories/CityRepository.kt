a5183898d1d309c61d5cd5a62b17cec2c938b9cd
Oussama Chatri <153307239+osamachatri@users.noreply.github.com>
2024-12-14 11:18:19 +0100

    Update CityRepository.kt
    Change CIty repository on data layer

--- app/src/main/java/com/oussama_chatri/weather/data/repositories/CityRepository.kt
+++ app/src/main/java/com/oussama_chatri/weather/data/repositories/CityRepository.kt
@@ -1,22 +1,2 @@
 package com.oussama_chatri.weather.data.repositories
 
-import com.oussama_chatri.weather.data.local.daos.CityDao
-import com.oussama_chatri.weather.data.local.entities.City
-import com.oussama_chatri.weather.data.remote.api.citiesApi.CityApiService
-
-class CityRepository(private val cityDao: CityDao, private val cityApiService: CityApiService) :
-    CityDao {
-    override suspend fun upsert(city: City) = cityDao.upsert(city)
-
-    override suspend fun deleteCityById(id: Int) = cityDao.deleteCityById(id)
-
-    override suspend fun getAllCities(): List<City> = cityDao.getAllCities()
-
-    override suspend fun getCityById(id: Int) = cityDao.getCityById(id)
-
-    suspend fun searchForCities(
-        namePrefix: String,
-    ) = cityApiService.searchCities(
-        namePrefix = namePrefix
-    )
-}
